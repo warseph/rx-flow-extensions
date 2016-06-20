42712920e24ac6da557038b7af412505cb1d1a6a
Ezequiel Rabinovich <erabinovich@gmail.com>
2016-06-20 15:04:47 -0400

    Add the `cached` method to the bundle

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ module.exports = libExt.bundle([
   /* eslint global-require: 0 */
   require('./extensions/just'),
   require('./extensions/take-while-inclusive'),
-  require('./extensions/polling')
+  require('./extensions/polling'),
+  require('./extensions/cached')
+
 ]);
